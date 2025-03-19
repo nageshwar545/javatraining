12865b77163f15c6e45e2edbaa04d368f7d92165
Nakka <nageshwar.nakka@cotiviti.com>
2025-03-19 13:35:50 +0530

    committing the missing files

---
+++ src/Assignment1/CompanyDetails.java
@@ -0,0 +1,24 @@
+package Assignment1;
+
+import java.util.Arrays;
+
+public class CompanyDetails {
+    public String companyName;
+    public String country;
+    public String[] models;
+
+    public CompanyDetails(String companyName, String country, String... models) {
+        this.companyName = companyName;
+        this.country = country;
+        this.models = models;
+        this.showCompanyDetails();
+    }
+    public void showCompanyDetails() {
+        System.out.println("Company Name"+ this.companyName);
+        System.out.println("Country"+ this.country);
+        System.out.print("Model(s) ");
+        for(String model: this.models) {
+            System.out.print(model+" ");
+        }
+    }
+}
--- src/Assignment1/Main.java
+++ src/Assignment1/Main.java
@@ -1,4 +1,13 @@
 package Assignment1;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Employee Details");
+        System.out.println("----------------");
+        new Employee(11322, "Gibbs");
+        new Employee(11324, "John", 29);
+        new Employee(11323, "Nick", 29, 10);
+        new Employee(11325, "Test", 29, 10, true);
+        new FetchDetails();
+    }
 }
